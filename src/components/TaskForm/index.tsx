4d13bb0980f6966441953e7c5532f8cd8d92aa16
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2022-08-11 00:33:13 -0300

    refactor: use useCallback hook on TaskForm component

--- src/components/TaskForm/index.tsx
+++ src/components/TaskForm/index.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent, useState, useCallback } from 'react';
 import { FiCheck } from 'react-icons/fi';
 import { useTask } from '../../context';
 
@@ -10,12 +10,21 @@ export function TaskForm(props: TaskFormProps) {
   const [description, setDescription] = useState('');
   const { addTask } = useTask();
 
-  const handleSubmit = (event?: FormEvent<HTMLFormElement>): void => {
-    event?.preventDefault();
+  const handleSubmit = useCallback(
+    (event?: FormEvent<HTMLFormElement>): void => {
+      event?.preventDefault();
 
-    addTask(description);
-    setDescription('');
-  };
+      const isValidDescription = description?.trim().length > 0;
+
+      if (!isValidDescription) {
+        return;
+      }
+
+      addTask(description);
+      setDescription('');
+    },
+    [addTask, description]
+  );
 
   return (
     <form className={styles.form} onSubmit={handleSubmit}>
